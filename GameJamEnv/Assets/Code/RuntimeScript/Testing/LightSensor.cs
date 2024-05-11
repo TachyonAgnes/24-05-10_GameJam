3d00870fe762c6de2a3220c0ab6bdb23768fdcc4
TachyonAgnes <para.bola@qq.com>
2024-05-10 21:25:46 -0400

    Update

---
+++ GameJamEnv/Assets/Code/RuntimeScript/Testing/LightSensor.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class LightSensor : MonoBehaviour
+{
+    [SerializeField]
+    private AdaptiveProbeVolumes _adaptiveProbeVolumes;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //void Start()
+    //{
+    //    if(_adaptiveProbeVolumes != null){
+    //        Vector3 sensorPosition = transform.position;
+
+    //        SphericalHarmonicsL2 sh;
+
+    //        _adaptiveProbeVolumes.
+    //    }
+    //}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
