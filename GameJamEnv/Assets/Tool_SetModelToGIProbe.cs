882e24e28bea06a1303b3b46c363d0b2427ad5f9
TachyonAgnes <para.bola@qq.com>
2024-05-12 01:18:29 -0400

    add playground

---
+++ GameJamEnv/Assets/Tool_SetModelToGIProbe.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Tool_SetModelToGIProbe : MonoBehaviour
+{
+    [ContextMenu("SetModelToGIProbe")]
+    public void SetModelToGIProbe(){
+        // find all child that has mesh renderer
+        MeshRenderer[] meshRenderers = GetComponentsInChildren<MeshRenderer>();
+        // set them contribute to GI
+        foreach (MeshRenderer meshRenderer in meshRenderers)
+        {
+            meshRenderer.gameObject.isStatic = true;
+            meshRenderer.receiveGI = ReceiveGI.LightProbes;
+        }
+    }
+}
